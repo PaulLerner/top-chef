2e935ef11e344cfca0f85561747282fea4151378
Unknown <paul.lerner@devinci.fr>
2018-02-08 17:05:51 +0100

    feat(scraping): scrapes all of the restaurants titles

--- michelin.js
+++ michelin.js
@@ -2,27 +2,66 @@ var express = require('express');
 var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
-//recuperer le nombre de pages en bas de la première page
-    url = 'https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin';//first page research off all starred french restaurants
-// /page-2
-    request(url, function(error, response, html){
+
+
+var number_of_pages=1;
+var titles =[];
+   const url = 'https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin';//first page research off all starred french restaurants
+
+    request(url, function(error, response, html){//gets the first page titles plus the number of pages
         if(!error){
             var $ = cheerio.load(html);
 
-			var titles =[];
-            var title;
-			//, release, rating;
-            //var json = { title : "", release : "", rating : ""};
-
             $('.poi_card-display-title').filter(function(){
                 var data = $(this);
-                title = data.text();
-				titles.push(title);
-            })
+                var title = data.text();
+				titles.push(title.trim());
+            });
+			$('.mr-pager-item').filter(function(){
+                var data = $(this);
+                number_of_pages = data.children().first().text();
+			
+            });
 
           
         }
 		else
 			console.log(error);
-		console.log(titles);
-    })
+		
+console.log("numb of pages "+number_of_pages);
+    });
+
+function Scrape()//gets all the remaining pages' title
+{
+	
+	for(var i=2;i<= number_of_pages;i++)
+	{
+			var url2= url+"/page-"+i;
+			request(url2, function(error, response, html){
+			if(!error){
+				var $ = cheerio.load(html);
+
+				$('.poi_card-display-title').filter(function(){
+					var data = $(this);
+					var title = data.text();
+					titles.push(title.trim());
+				});
+
+
+			}
+			else
+				console.log(error);
+
+		});
+
+	}
+
+			
+
+}
+function Display()
+{
+	console.log(titles);
+}
+setTimeout(Scrape, 5000);//time out because Node JS is asynchronous and we have to get the number of pages
+setTimeout(Display,30000);//we have to scrape all of the restaurant titles
