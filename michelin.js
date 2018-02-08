c62496ab33f8cf749df65cc3edabc8dd76a0ef1e
Unknown <paul.lerner@devinci.fr>
2018-02-08 17:15:21 +0100

    feat(scraping): store the restaurant titles' array in a json file

--- michelin.js
+++ michelin.js
@@ -2,7 +2,7 @@ var express = require('express');
 var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
-
+var fs = require('fs');
 
 var number_of_pages=1;
 var titles =[];
@@ -62,6 +62,11 @@ function Scrape()//gets all the remaining pages' title
 function Display()
 {
 	console.log(titles);
+	var json = JSON.stringify(titles);
+	fs.writeFile('michelin.json', json, 'utf8', (err) => {
+	  if (err) throw err;
+
+	});
 }
 setTimeout(Scrape, 5000);//time out because Node JS is asynchronous and we have to get the number of pages
 setTimeout(Display,30000);//we have to scrape all of the restaurant titles
